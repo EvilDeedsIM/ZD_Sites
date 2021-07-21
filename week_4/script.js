10280513cdc41ca69e7ca6ab340db26201b43ec9
EvilDeedsIM <evildeedsim@gmail.com>
2021-07-21 17:03:24 +0300

    week_4_final

--- week_4/script.js
+++ week_4/script.js
@@ -1,8 +1,8 @@
+const body = document.querySelector("body");
 const items = document.querySelectorAll(".item");
 const images = document.querySelectorAll(".item__img");
 const shuffleBtn = document.querySelector(".shuffle-btn");
 
-console.log(images[1]);
 const imageLinks = [
   "https://images.unsplash.com/photo-1452696193712-6cabf5103b63?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
   "https://images.unsplash.com/photo-1546188994-07c34f6e5e1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1051&q=80",
@@ -11,6 +11,12 @@ const imageLinks = [
   "https://images.unsplash.com/photo-1553152531-b98a2fc8d3bf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80",
   "https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80",
 ];
+
+checkOrientation();
+
+window.addEventListener("resize", () => {
+  checkOrientation();
+});
 
 shuffleBtn.addEventListener("click", shuffleImages);
 
@@ -36,5 +42,13 @@ function shuffleImages() {
 
   for (let i = 0; i < images.length; i++) {
     images[i].src = imageLinks[i];
+  }
+}
+
+function checkOrientation() {
+  if (window.innerWidth < window.innerHeight) {
+    body.classList.add("vertical");
+  } else {
+    body.classList.remove("vertical");
   }
 }
